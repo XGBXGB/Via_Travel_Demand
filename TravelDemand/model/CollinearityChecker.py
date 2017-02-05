77d2a908f292a8ac35a43e71ba972c25aa5b0124
XGBXGB <xtian32395@yahoo.com>
2017-02-06 00:05:53 +0800

    Added stuff

--- TravelDemand/model/CollinearityChecker.py
+++ TravelDemand/model/CollinearityChecker.py
@@ -4,22 +4,20 @@ import statsmodels.formula.api as smf
 
 class CollinearityChecker:
     def __init__(self, pathToData):
-        self.pathToData = pathToData
+        self.data = pd.read_csv(pathToData, index_col=0)
 
     # get collinearity (r^2 value) of a pair of columns
     def getCollinearityScore(self, dependentCol, independentCol):
-        data = pd.read_csv(self.pathToData, index_col=0)
-        df2 = data.iloc[:, [independentCol, dependentCol]]
+        df2 = self.data.iloc[:, [independentCol, dependentCol]]
         lm = smf.ols(formula='Sales ~ TV', data=df2).fit()
         return lm.rsquared
 
     # print collinearities (r^2 value) of each pair of columns
     def printAllCollinearities(self):
-        data = pd.read_csv(self.pathToData, index_col=0)
-        length_col = data.shape[1]
+        length_col = self.data.shape[1]
         while(0 != length_col-1):
             for x in range(0, length_col-1):
-                df2 = data.iloc[:, [x, length_col-1]]
+                df2 = self.data.iloc[:, [x, length_col-1]]
                 col_name1 = df2.columns.values[0]
                 col_name2 = df2.columns.values[1]
                 lm = smf.ols(formula=col_name1+" ~ "+col_name2, data=df2).fit()
@@ -27,10 +25,30 @@ class CollinearityChecker:
             length_col -= 1
 
     def getInterceptValues(self, col_names, dependent_col_name):
-        data = pd.read_csv(self.pathToData, index_col=0)
-        df2 = data.loc[:, col_names]
-        col_names_string = " + ".join(col_names)
-        lm = smf.ols(formula=str(self.dependent_col_name) + " ~ " + str(col_names_string), data=df2).fit()
+        intercept_values = []
+        # K-FOLD CROSS VALIDATION
+        zone_numbers = self.data.index.values
+        testing_zone_nums = []
+
+        # implement outer loop for k-fold validation
+        for zone_num in zone_numbers:
+            if zone_num not in testing_zone_nums:
+                df2 = self.data.loc[:, col_names]
+                col_names_string = " + ".join(col_names)
+                lm = smf.ols(formula=str(dependent_col_name) + " ~ " + str(col_names_string), data=df2).fit()
+                intercept_values.append(lm.params.values)
+
+        num_of_rows = len(intercept_values)
+        num_of_params = len(lm.params.values)
+        # avg_of_params = [0] * num_of_params
+        total_sum_of_params = [sum(x) for x in zip(*intercept_values)]
+        avg_of_params = [x / num_of_rows for x in total_sum_of_params]
+
+
+
+
+
+
 
         return lm.params.values
 
